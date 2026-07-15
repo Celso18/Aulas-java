46c217924e6ad4e38526f838c1d845133a5b89d1
Celso Romão <celsoromao9@gmail.com>
2026-07-15 20:55:40 -0300

    Atualização dos códigos

---
+++ src/Aulas/QuintaAula.java
@@ -0,0 +1,18 @@
+package Aulas;
+
+public class QuintaAula {
+    public static void main(String [] args) {
+        int[] numeros = new int[10];
+
+
+
+        for (int i=0; i<numeros. length; i++{
+            System.out.println(numeros [i]);
+        }
+
+        for (int numero. numeros);{
+            System.out.println(numeros);
+    }
+
+}
+
