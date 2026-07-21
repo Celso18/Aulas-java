6d2f117c93d143db1b8b421976524c74b6977816
Celso Romão <celsoromao9@gmail.com>
2026-07-21 19:20:36 -0300

    Atividade 7

--- src/Aulas/encapsulamento/banco/principal/Main.java
+++ src/Aulas/encapsulamento/banco/principal/Main.java
@@ -1,4 +1,22 @@
 package Aulas.encapsulamento.banco.principal;
 
+import Aulas.encapsulamento.banco.modelo.ContaBancaria;
+
 public class Main {
+    public static void main(String[] args) {
+        // Linha 14 corrigida: Passando apenas o nome
+        ContaBancaria minhaConta = new ContaBancaria("Celso");
+
+        // Remova a linha "minhaConta.setTitular(...)" pois ela causará erro agora (Regra 2)
+
+        System.out.println(minhaConta.getTitular());
+        System.out.println("Número da conta: " + minhaConta.getNumeroDaConta());
+
+        // Linhas 21 e 22: Agora vão funcionar perfeitamente sem erro!
+        ContaBancaria pessoal = new ContaBancaria("Fulano");
+        ContaBancaria pessoal2 = new ContaBancaria("Robso");
+
+        System.out.println("Conta pessoal 1: " + pessoal.getNumeroDaConta());
+        System.out.println("Conta pessoal 2: " + pessoal2.getNumeroDaConta());
+    }
 }
