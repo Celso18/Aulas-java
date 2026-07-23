490d135e7be1925b880ce6054cfbc5a957a4c22b
Celso Romão <celsoromao9@gmail.com>
2026-07-22 21:30:12 -0300

    Atividade 7 atualização

--- src/Aulas/heranca/locomocao/Main.java
+++ src/Aulas/heranca/locomocao/Main.java
@@ -2,14 +2,5 @@ package Aulas.heranca.locomocao;
 
 public class Main {
     public static void main(String[] args) {
-
-        Carro carro = new Carro();
-        Moto moto = new MotoACombustao();
-
-
-        carro.setNomeVeículo = "Celtinha";
-        carro.ligar();
-        carro.ligarArCondicionado();
-
     }
 }
